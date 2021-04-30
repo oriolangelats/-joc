701797ff5f0b6a5e20bd91117d730b021543839f
oriolangelats <83455474+oriolangelats@users.noreply.github.com>
2021-04-30 14:36:21 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    if (sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    game.pause()
+    basic.showNumber(game.score())
+    basic.pause(500)
+    game.resume()
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 2)
+basic.forever(function () {
+    sprite.move(1)
+    basic.pause(100)
+    if (sprite.isTouchingEdge()) {
+        sprite.ifOnEdgeBounce()
+    }
+})
